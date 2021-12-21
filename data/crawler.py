e63c80afa038c4c7a6f85b3f3b60f664fc38461a
eyedi <gdregas18@gmail.com>
2021-12-22 08:16:31 +0900

    re-sorting dir, files
    사용하지 않는 파일, 이동된 파일 삭제
    pip freeze 추가
    Crawler project Start

---
+++ data/crawler.py
@@ -0,0 +1,13 @@
+# !python 
+# encoding: UTF-8
+import time
+import traceback
+import os
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+
