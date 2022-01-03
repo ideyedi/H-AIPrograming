4ebfe269e44250c69fe707ae033e4db9d3d2da64
ideyedi <gdregas18@gmail.com>
2022-01-03 23:31:28 +0900

    crawler 구분

---
+++ data/naver_crawler.py
@@ -0,0 +1,32 @@
+# !python 
+# encoding: UTF-8
+import time
+import traceback
+import os
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+'''
+webDrv
+targetURL
+'''
+webDrv = '/usr/bin/safaridriver'
+targetURL = 'https://shopping.naver.com/home/p/index.naver'
+
+#opt = webdriver.SafariOptions()
+#opt.add_argument('--incognito')
+
+drv = webdriver.Safari(executable_path=webDrv)
+
+try:
+    drv.get(targetURL)
+    category = drv.find_elements(By.CLASS_NAME, 'btn_cate_all')
+    category[0].click()
+
+except:
+    traceback.print_exc()
+
