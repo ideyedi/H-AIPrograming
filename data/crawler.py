a514ccbdde288f9fce39b7480bbce3e3a9dde360
지은석 <esji@wemakeprice.com>
2022-01-03 09:44:59 +0900

    safari driver enable

--- data/crawler.py
+++ data/crawler.py
@@ -10,4 +10,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
+'''
+webDrv
+targetURL
+'''
+webDrv = '/usr/bin/safaridriver'
+targetURL = 'https://shopping.naver.com/home/p/index.naver'
+
+#opt = webdriver.SafariOptions()
+#opt.add_argument('--incognito')
+
+drv = webdriver.Safari(executable_path=webDrv)
+
+try:
+    drv.get(targetURL)
+
+except:
+    traceback.print_exc()
 
