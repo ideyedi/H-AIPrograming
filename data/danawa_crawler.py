c3bd1c12ec08f2e6ec572c72ed2363272e604591
ideyedi <gdregas18@gmail.com>
2022-01-04 09:48:22 +0900

    update danawa crawler

--- data/danawa_crawler.py
+++ data/danawa_crawler.py
@@ -16,18 +16,37 @@ targetURL
 '''
 webDrv = '/usr/bin/safaridriver'
 targetURL = 'http://www.danawa.com'
-#targetURL = 'https://shopping.naver.com/home/p/index.naver'
-
 #opt = webdriver.SafariOptions()
 #opt.add_argument('--incognito')
 
 drv = webdriver.Safari(executable_path=webDrv)
+herf_list = []
 
 try:
     drv.get(targetURL)
     category = drv.find_elements(By.CLASS_NAME, 'btn_cate_all')
     category[0].click()
 
+    # Sleep click event
+    time.sleep(3)
+    detail_list = drv.find_elements(By.CLASS_NAME, 'category-all__detail__list')
+    print('parsing category list : {}'.format(len(detail_list)))
+
+    # Get link list
+    for items in detail_list:
+        get_items = items.find_elements(By.CLASS_NAME, 'item')
+        a_tag = get_items[0].find_elements(By.TAG_NAME, 'a')
+        herf_list.append(a_tag[0].get_attribute('href'))
+        #print('len {}'.format(len(get_items)))
+
+    print('Link count : {}'.format(len(herf_list)))
+    # Access to fisrt link 
+    drv.get(herf_list[0])
+
 except:
     traceback.print_exc()
 
+finally:
+    time.sleep(3)
+    drv.quit()
+    print('Quit crawlering')
