a81412f8acb6c43538153851065868d92e35ae0e
ideyedi <gdregas18@gmail.com>
2022-03-13 15:32:26 +0900

    변수 확인 용

--- util/GetData.py
+++ util/GetData.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import os
+
 
 class GetData():
+
     def __init__(self):
         '''
         한번에 모든 파일을 읽어서 가지고 올 수 있는 방안 마련
         '''
-        df2144 = pd.read_csv('../data/220117.2144.csv', encoding='UTF-8')
-        df2204 = pd.read_csv('../data/220117.2204.csv', encoding='UTF-8')
+        self.file_list = []
+        self.except_list = ['.DS_Store', 'README.md']
+        #df2144 = pd.read_csv('../data/220117.2144.csv', encoding='UTF-8')
+        #df2204 = pd.read_csv('../data/220117.2204.csv', encoding='UTF-8')
+        for filename in os.listdir('data'):
+            if filename not in self.except_list:
+                self.file_list.append(filename)
 
+        #print(f"{self.file_list}")
---
+++ util/test.py
@@ -0,0 +1,5 @@
+import GetData as gd
+
+
+getData = gd.GetData()
+getData.file_list
