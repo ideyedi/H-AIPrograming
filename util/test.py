10b34efa77f4fd276a8841d12e3609396ea4de7d
esji <esji@wemakeprice.com>
2022-03-19 23:10:12 +0900

    develop
    test code 추가
    test code 위치 변경, 다른폴더 참조하는 방법 추가
    platform 정보 yaml 추가

--- util/test.py
+++
@@ -1,8 +0,0 @@
-import GetData as gd
-
-
-getData = gd.GetData()
-# Instance variables 테스트
-# print(f"{gd.GetData.file_list}")
-print(f"Instance variables: {getData.file_list}")
-print(f"Class variables: {gd.GetData.test_list}")
