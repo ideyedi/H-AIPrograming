ecce1a29330c44cb97a38be040ed3b9300b91886
esji <esji@wemakeprice.com>
2022-03-21 23:03:29 +0900

    data 한번에 가지고 오는 함수 추가

--- util/GetData.py
+++ util/GetData.py
@@ -10,6 +10,7 @@ class GetData:
         한번에 모든 파일을 읽어서 가지고 올 수 있는 방안 마련
         '''
         self.file_list = []
+        self.concated_list = []
         self.except_list = ['.DS_Store', 'README.md']
         #df2144 = pd.read_csv('../data/220117.2144.csv', encoding='UTF-8')
         #df2204 = pd.read_csv('../data/220117.2204.csv', encoding='UTF-8')
@@ -17,9 +18,13 @@ class GetData:
             if filename not in self.except_list:
                 self.file_list.append(filename)
 
-    def merge_csv_file(self):
+        for item in self.file_list:
+            tempFrame = pd.read_csv('./data/'+item, encoding='UTF-8')
+            self.concated_list.append(tempFrame)
 
-        return 0
+    def getResult(self):
+        df = pd.concat(self.concated_list, axis=0, ignore_index=True)
+        return df
 
 
 if __name__=="__main__":
@@ -32,3 +37,5 @@ if __name__=="__main__":
     # print(f"{gd.GetData.file_list}")
     print(f"Instance variables: {getData.file_list}")
     print(f"Class variables: {GetData.test_list}")
+
+    print(getData.getResult().describe())
